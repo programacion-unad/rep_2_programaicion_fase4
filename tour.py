60642556c710f070a456a18e3123a913478f3971
lranngelB <134731792+lranngelB@users.noreply.github.com>
2026-05-06 21:29:56 -0500

    Delete tour.py

--- tour.py
+++
@@ -1,8 +0,0 @@
-from servicio import Servicio
-
-class Tour(Servicio):
-    def calcular_costo(self, personas):
-        return personas * 50
-    
-    def descripcion(self):
-        return "servicio turistico"
