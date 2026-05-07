12dda90b24133000858c68e30f4823ad68410f31
lranngelB <134731792+lranngelB@users.noreply.github.com>
2026-05-06 21:30:16 -0500

    Delete transporte.py

--- transporte.py
+++
@@ -1,8 +0,0 @@
-from servicio import Servicio
-
-class Transporte(Servicio):
-    def calcular_costo(self, distancia):
-        return distancia * 2
-    
-    def descripcion(self):
-        return "servicio de transporte"
