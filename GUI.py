8f5cb9e8bf07a0ab94b42b81d4f0e2a9e17274e5
lranngelB <lrangelbeltran@gmail.com>
2026-05-08 16:00:24 -0500

    luis: coneccion de clase cliente

--- GUI.py
+++ GUI.py
@@ -1,8 +1,11 @@
 import tkinter as tk
 from tkinter import ttk  
 from tkinter import messagebox
-
+from coneccion import funciones_clientes
 class AplicacionSJF(tk.Tk):
+    
+    func = funciones_clientes()
+    
     def __init__(self):
         super().__init__()
         self.title("Software FJ")
@@ -36,14 +39,19 @@ class AplicacionSJF(tk.Tk):
         self.ent_nombre.grid(row=0, column=1, padx=5, pady=5)
         
         tk.Label(frame_form, text="Correo:").grid(row=1, column=0, padx=5, pady=5)
-        self.ent_nombre = ttk.Entry(frame_form)
-        self.ent_nombre.grid(row=1, column=1, padx=5, pady=5)
+        self.ent_correo = ttk.Entry(frame_form)
+        self.ent_correo.grid(row=1, column=1, padx=5, pady=5)
 
         ttk.Button(self.tab_clientes, text="Guardar Cliente", command=self.guardar_datos).pack(pady=20)
 
     def guardar_datos(self):
+        
         nombre = self.ent_nombre.get()
-        if nombre:
+        correo = self.ent_correo.get()
+                
+        self.func.guadar_dt(nombre, correo)               
+        
+        if nombre and correo:
             messagebox.showinfo("Éxito", f"Cliente {nombre} registrado localmente.")
         else:
             messagebox.showwarning("Atención", "El campo nombre es obligatorio.")
@@ -51,3 +59,4 @@ class AplicacionSJF(tk.Tk):
 if __name__ == "__main__":
     app = AplicacionSJF()
     app.mainloop()
+    
